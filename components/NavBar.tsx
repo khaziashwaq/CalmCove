a5f383796ebbe25c3122027cf4de6634f7c70f6f
khaziashwaq <ashwaqkhazi1729@gmail.com>
2024-05-18 13:44:02 +0530

    initial commit

---
+++ components/NavBar.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+const NavBar = () => {
+    return(
+        <div>
+          hello
+        </div>
+        )
+}
+
+export default NavBar;
---
+++ components/index.ts
@@ -0,0 +1,5 @@
+import Hero from "./Hero";
+import NavBar from "./NavBar";
+
+export {Hero}
+export {NavBar}
